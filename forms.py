3dd3ccc59a512739c4b0b6636bf15793ab7ae83a
ANANAzZzZz <vladscooltrol@gmail.com>
2024-03-27 18:59:47 +0300

    add forms.py

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
+
